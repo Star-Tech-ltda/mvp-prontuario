85d344a0d8fb224fe13699b4f95b35df931317f1
Ruan Pablo <ruanpablodnzzz@gmail.com>
2025-04-23 15:02:42 -0300

    feat(service): fazendo a logica para calculo e interpretacao de pressao arterial

--- app/Services/MetricInterpreterService.php
+++ app/Services/MetricInterpreterService.php
@@ -36,11 +36,24 @@ class MetricInterpreterService
 
 
         //pressão arterial
+        if (!empty($data['systolic_pressure']) && !empty($data['diastolic_pressure'])) {
+            $systolic = intval($data['systolic_pressure']);
+            $diastolic = intval($data['diastolic_pressure']);
 
+            if (!CalculatedMetric::where('evolution_id', $evolutionId)
+                ->where('calculated_type', MetricType::BP)
+                ->exists()) {
 
+                $interpretation = self::interpretBP($systolic, $diastolic);
 
-
-
+                CalculatedMetric::create([
+                    'evolution_id' => $evolutionId,
+                    'calculated_type' => MetricType::BP,
+                    'result' => "{$systolic}/{$diastolic}",
+                    'interpretation' => $interpretation,
+                ]);
+            }
+        }
 
 
 
@@ -72,6 +85,7 @@ class MetricInterpreterService
     }
 
 
+    //imc
     private static function calculateBMI(float $width, float $height): float
     {
         if ($height > 3){
@@ -80,7 +94,6 @@ class MetricInterpreterService
         return $width / ($height * $height);
 
     }
-
     private static function interpretBmi(float $bmi): string
     {
         return match (true) {
@@ -91,6 +104,21 @@ class MetricInterpreterService
             $bmi < 40.0 => 'Obesidade Grau II',
             $bmi >= 40.0 => 'Obesidade Grau III',
             default => 'Acima dos intervalos categorizados',
+        };
+    }
+
+    // pressao arterial
+    private static function interpretBP(int $systolic, int $diastolic): string
+    {
+        return match (true) {
+            $systolic < 120 && $diastolic < 80 => 'Ótima',
+            $systolic < 130 && $diastolic < 85 => 'Normal',
+            $systolic < 140 && $diastolic < 90 => 'Normal-alta',
+            $systolic < 160 && $diastolic < 100 => 'Hipertensão estágio 1',
+            $systolic < 180 && $diastolic < 110 => 'Hipertensão estágio 2',
+            $systolic >= 180 || $diastolic >= 110 => 'Hipertensão estágio 3',
+            $systolic >= 140 && $diastolic < 90 => 'Hipertensão sistólica isolada',
+            default => 'Indeterminado',
         };
     }
 }
