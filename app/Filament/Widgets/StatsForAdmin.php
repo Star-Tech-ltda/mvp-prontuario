81e1e6d82f63e44bc07721aa0e1ead7318975aca
Ruan Pablo <ruanpablodnzzz@gmail.com>
2025-05-12 17:04:05 -0300

    feat(widgets)

---
+++ app/Filament/Widgets/StatsForAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Budget;
+use App\Models\Patient;
+use App\Models\User;
+use Filament\Widgets\ChartWidget;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Illuminate\Support\Facades\Auth;
+
+class StatsForAdmin extends BaseWidget
+{
+    public static function canView(): bool
+    {
+        return auth()->check() && auth()->user()->isAdmin();
+    }
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Usuários', User::count())
+                ->description('Total de Usuários Cadastrados')
+                ->icon('heroicon-c-user-circle'),
+        ];
+    }
+}
